ae4c36463d18d2b8b56f8b07f822af629dd238f9
Shabin-k <helloshabink@gmail.com>
2024-06-29 14:45:30 +0530

    feat : add authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 
 import "./globals.css";
 import Navbar from "@/components/Navbar";
@@ -20,11 +21,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} min-w-[350px]`}>
-        <Navbar />
-        {children}
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} min-w-[350px]`}>
+          <Navbar />
+          {children}
         </body>
-    </html>
+      </html>
+    </ClerkProvider>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,15 @@
 import Link from "next/link";
 import Image from "next/image";
+import { SignOutButton } from "@clerk/nextjs";
+import { BadgePlus, LogOut } from "lucide-react";
+import { currentUser } from "@clerk/nextjs/server";
 
 import logo from "@/assets/logo.ico";
 import { Button } from "./ui/button";
 
-export default function Navbar() {
+export default async function Navbar() {
+  const user = await currentUser();
+
   return (
     <header className="shadow-sm">
       <nav className="m-auto flex max-w-5xl items-center justify-between px-3 py-5">
@@ -12,9 +17,30 @@ export default function Navbar() {
           <Image src={logo} width={40} height={40} alt="Flow Jobs logo" />
           <span className="text-xl font-bold tracking-tight">Horecah Jobs</span>
         </Link>
-        <Button asChild>
-          <Link href="/jobs/new">Post a job</Link>
-        </Button>
+        <div className="hidden flex-row space-x-1 md:flex">
+          <Button asChild>
+            <Link href="/jobs/new">Post a job</Link>
+          </Button>
+          {user && (
+            <SignOutButton>
+              <Button>log out</Button>
+            </SignOutButton>
+          )}
+        </div>
+        <div className="flex flex-row space-x-1 md:hidden">
+          <Button className="rounded-full">
+            <Link href="/jobs/new">
+              <BadgePlus size={20} />
+            </Link>
+          </Button>
+          {user && (
+            <SignOutButton>
+              <Button className="rounded-full">
+                <LogOut size={20} />
+              </Button>
+            </SignOutButton>
+          )}
+        </div>
       </nav>
     </header>
   );
